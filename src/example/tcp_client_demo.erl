67b9cf912cf724da1be4f81638ab919a525d0b16
ziyouchutuwenwu <ziyouchutuwenwu@gmail.com>
2021-01-06 21:34:58 +0800

    1

--- src/example/tcp_client_demo.erl
+++ src/example/tcp_client_demo.erl
@@ -3,8 +3,8 @@
 -export([start/0, send_by_socket/1, send_by_pid/1]).
 
 start() ->
-  ClientsNumber = 1,
-  tcp_client:start("demo_name", {"127.0.0.1", 9999}, ClientsNumber, demo_client_socket_behavior_config_impl).
+  ClientNumber = 1,
+  tcp_client:start("demo_name", {"127.0.0.1", 9999}, ClientNumber, demo_client_socket_behavior_config_impl).
 
 send_by_socket(Socket) ->
   tcp_client_send:send_data_by_socket(Socket, 111, <<"msg send by socket">>, demo_client_socket_behavior_config_impl).
