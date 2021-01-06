919ae566c2cf66593c4d092875fddcaeb77a399a
ziyouchutuwenwu <ziyouchutuwenwu@gmail.com>
2021-01-06 21:20:42 +0800

    去掉无意义的注释

--- src/example/tcp_client_demo.erl
+++ src/example/tcp_client_demo.erl
@@ -2,10 +2,9 @@
 
 -export([start/0, send_by_socket/1, send_by_pid/1]).
 
-%% 需要的时候，手动调用
 start() ->
   ClientsNumber = 1,
-  tcp_client:start("demo_name", {"192.168.88.92", 9999}, ClientsNumber, demo_client_socket_behavior_config_impl).
+  tcp_client:start("demo_name", {"127.0.0.1", 9999}, ClientsNumber, demo_client_socket_behavior_config_impl).
 
 send_by_socket(Socket) ->
   tcp_client_send:send_data_by_socket(Socket, 111, <<"msg send by socket">>, demo_client_socket_behavior_config_impl).
