73cb3c8894304b0d16195d3c0fb9e4ce51d22799
ziyouchutuwenwu <ziyouchutuwenwu@gmail.com>
2022-11-22 14:42:16 +0800

    .

--- src/example/behavior_impl/client_socket_behavior_config_impl.erl
+++ src/example/behavior_impl/client_socket_behavior_config_impl.erl
@@ -1,6 +1,6 @@
 -module(client_socket_behavior_config_impl).
 
--export([get_socket_handler_module/0, get_socket_package_module/0, get_socket_options_module/0]).
+-export([get_socket_handler_module/0, get_socket_package_module/0]).
 
 get_socket_handler_module() ->
   demo_client_socket_handler_impl.
@@ -8,6 +8,3 @@ get_socket_handler_module() ->
 %% 业务级别拆包
 get_socket_package_module() ->
   demo_client_socket_package_impl.
-
-get_socket_options_module() ->
-  demo_client_options_impl.
