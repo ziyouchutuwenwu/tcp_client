aa21cb3183b8108bf0ead62573de214bf5be1afa
ziyouchutuwenwu <ziyouchutuwenwu@gmail.com>
2025-11-12 17:13:14 +0800

    .

--- src/tcp_client_app.erl
+++ src/tcp_client_app.erl
@@ -1,8 +1,3 @@
-%%%-------------------------------------------------------------------
-%% @doc tcp_client public API
-%% @end
-%%%-------------------------------------------------------------------
-
 -module(tcp_client_app).
 
 -behaviour(application).
@@ -14,5 +9,3 @@ start(_StartType, _StartArgs) ->
 
 stop(_State) ->
     ok.
-
-%% internal functions
